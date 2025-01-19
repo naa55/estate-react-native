8109e6d249346ced2b84b9e025033b66eb6cbfeb
Naa55 <bettyablorh777@gmail.com>
2025-01-19 09:24:43 +0000

    my app

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,126 @@
+import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
+import NoResult from "@/components/NoResult";
+import Search from "@/components/search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { getLatestProperties, getProperties } from "@/lib/appwrite";
+import { useGlobalContext } from "@/lib/globalProvider";
+import seed from "@/lib/seed";
+import { useAppwrite } from "@/lib/useAppwrite";
+import { Link, router, useLocalSearchParams } from "expo-router";
+import { useEffect } from "react";
+import { ActivityIndicator, Button, FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+// import "../global.css";
+
+export default function Index() {
+  const {user} = useGlobalContext();
+  const params = useLocalSearchParams<{ query?: string; filter?: string }>();
+
+
+  const { data: latestProperties, loading: latestPropertiesLoading } =
+    useAppwrite({
+      fn: getLatestProperties,
+    });
+
+  const {
+    data: properties,
+    refetch,
+    loading,
+  } = useAppwrite({
+    fn: getProperties,
+    params: {
+      filter: params.filter!,
+      query: params.query!,
+      limit: 6,
+    },
+    skip: true,
+  });
+
+  useEffect(() => {
+    refetch({
+      filter: params.filter!,
+      query: params.query!,
+      limit: 6,
+    });
+  }, [params.filter, params.query]);
+
+  const handleCardPress = (id: string) => router.push(`/properties/${id}`);
+
+
+  return (
+    <SafeAreaView className="bg-white h-full">
+      {/* <Button title='Seed' onPress={seed}/> */}
+      <FlatList data={properties} contentContainerClassName="pb-32" columnWrapperClassName="flex gap-5 px-5" showsVerticalScrollIndicator={false}
+      ListEmptyComponent={
+        loading ? (
+          <ActivityIndicator size='large' className="text-primary-300 mt-5"/>
+        ) :<NoResult/>
+      }
+      renderItem={({item}) => <Card item={item} onPress={() => handleCardPress(item.$id)}/>} keyExtractor={(item) => item.$id} numColumns={2} ListHeaderComponent={
+          <View className="px-5">
+          <View className="flex flex-row items-center justify-between mt-5">
+            <View className="flex flex-row">
+              <Image source={{uri: user?.avatar}} className="size-12 rounded-full" />
+              <View className="flex flex-col items-start ml-2 justify-center">
+                <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
+                <Text className="text-base font-rubik-medium text-black-300">{user?.name}</Text>
+              </View>
+            </View>
+            <Image source={icons.bell} className="size-6" />
+          </View>
+          <Search />
+          <View className="my-5">
+            <View className="flex flex-row items-center justify-between">
+              <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
+              <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+              </TouchableOpacity>
+            </View>
+
+            {/* {}
+          <FlatList renderItem={({item}) => <FeaturedCard item={item} onPress={() => handleCardPress(item.$id)}/>} data={latestProperties} keyExtractor={(item) => item.$id} horizontal bounces={false} showsHorizontalScrollIndicator={false} contentContainerClassName="flex gap-5 mt-5"/> */}
+          {latestPropertiesLoading ? (
+                <ActivityIndicator size="large" className="text-primary-300" />
+              ) : !latestProperties || latestProperties.length === 0 ? (
+                <NoResult />
+              ) : (
+                <FlatList
+                  data={latestProperties}
+                  renderItem={({ item }) => (
+                    <FeaturedCard
+                      item={item}
+                      onPress={() => handleCardPress(item.$id)}
+                    />
+                  )}
+                  keyExtractor={(item) => item.$id}
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  contentContainerClassName="flex gap-5 mt-5"
+                />
+              )}
+       
+          </View>
+  
+          <View className="flex flex-row items-center justify-between">
+              <Text className="text-xl font-rubik-bold text-black-300">Our Recommandation</Text>
+              <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+              </TouchableOpacity>
+            </View>
+            <Filters/>
+
+  {/* 
+          <FeaturedCard />
+          <Card /> */}
+  
+        </View>
+      }/>
+    
+  
+      {/* <FeaturedCard/>
+      <Card/> */}
+    </SafeAreaView>
+  );
+}
---
+++ app/sign-in.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Alert, Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import images from '@/constants/images'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import icons from '@/constants/icons'
+import { login } from '@/lib/appwrite'
+import { Redirect } from 'expo-router'
+import { useGlobalContext } from '@/lib/globalProvider'
+
+const SignIn = () => {
+    const { refetch, loading, isLogged } = useGlobalContext();
+
+    if (!loading && isLogged) return <Redirect href="/" />;
+    const handleLogin = async () => {
+        const result = await login();
+        console.log(result)
+        if (result) {
+          refetch();
+        } else {
+            <Redirect href="/" />
+          Alert.alert("Error", "Failed to login");
+        }
+      };
+    return (
+        <SafeAreaView className='bg-white h-full'>
+            <ScrollView contentContainerClassName='h-full'>
+                <Image source={images.onboarding} className='w-full h-4/6' resizeMode='contain' />
+                <View className='px-10'>
+                    <Text className='text-base text-center uppercase font-rubik text-black-200'>Welcome to ReState</Text>
+                    <Text className='text-3xl font-rubik-bold text-black-200 text-center mt-2'>
+                        Let's Get You Close to {"\n"}
+                        <Text className='text-primary-300'>Your Ideal Home</Text>
+                    </Text>
+                    <Text className='text-lg font-rubik text-black-200 text-center mt-12'>
+                        Login to ReState with Google
+                    </Text>
+                        <TouchableOpacity onPress={handleLogin} className='bg-hite shadow-m shadow-zinc-300 rounded-full w-full py-4 mt-5'>
+                        <View className='flex flex-row items-center justify-center'>
+                            <Image source={icons.google} className='w-5 h-5' resizeMode='contain' />
+                            <Text className='text-lg font-rubik-medium text-black-300 ml-3 mt-1'>Continue with Google</Text>
+                            </View>
+                        </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+export default SignIn
